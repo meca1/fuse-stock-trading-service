4640004baaf59c9d246ca55b258e61bf3b6706e6
meca1 <drramosdev@gmail.com>
2025-04-26 14:25:57 -0500

    Add pagination fields to stocks table in migration script

---
+++ db/migrations/20250426095000_add_pagination_fields.sql
@@ -0,0 +1,9 @@
+-- Agregar campos de paginación a la tabla stocks
+ALTER TABLE stocks ADD COLUMN IF NOT EXISTS page_token TEXT;
+ALTER TABLE stocks ADD COLUMN IF NOT EXISTS page_number INTEGER;
+ALTER TABLE stocks ADD COLUMN IF NOT EXISTS exchange VARCHAR(50);
+
+-- Actualizar el comentario para la columna page_token
+COMMENT ON COLUMN stocks.page_token IS 'Token de paginación para acceder rápidamente a esta acción (formato base64)';
+COMMENT ON COLUMN stocks.page_number IS 'Número de página donde se encuentra la acción';
+COMMENT ON COLUMN stocks.exchange IS 'Bolsa donde se negocia la acción';
