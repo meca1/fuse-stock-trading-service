ef566163818f370ccb866defcfc80d1828477b8f
meca1 <drramosdev@gmail.com>
2025-04-27 02:37:19 -0500

    feat(db): add total_value and last_updated columns to portfolios table

---
+++ db/migrations/20250427022000_add_portfolio_value_and_last_updated.sql
@@ -0,0 +1,7 @@
+-- migrate:up
+ALTER TABLE portfolios ADD COLUMN total_value NUMERIC DEFAULT 0;
+ALTER TABLE portfolios ADD COLUMN last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+
+-- migrate:down
+ALTER TABLE portfolios DROP COLUMN total_value;
+ALTER TABLE portfolios DROP COLUMN last_updated; 
