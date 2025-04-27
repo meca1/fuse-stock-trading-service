24f2cd2caf82ce7d1c590a6f668f50196604c2cf
meca1 <drramosdev@gmail.com>
2025-04-27 14:26:02 -0500

    feat(model): add IPortfolioSummary interface to represent portfolio summary

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -54,3 +54,12 @@ export interface ITransaction {
   created_at?: Date;
   updated_at?: Date;
 }
+
+export interface IPortfolioSummary {
+  stock_symbol: string;
+  quantity: number;
+  current_price: number;
+  total_cost: number;
+  current_value: number;
+  profit_loss: number;
+}
